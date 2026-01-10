2f6e131672cfbf51cda091234ee7a27a2b911d40
maha <me58@alu.ua.es>
2026-01-10 17:31:19 +0100

    middleware para que solo los usuarios logeados pueden reservar

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ReservaController;
 use App\Http\Controllers\TratamientoController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +9,11 @@ Route::view('/', 'welcome');
 // Public routes for treatments
 Route::get('/tratamientos', [TratamientoController::class, 'index'])->name('tratamientos.index');
 Route::get('/tratamientos/{tratamiento}', [TratamientoController::class, 'show'])->name('tratamientos.show');
+
+// Reservation routes - requires authentication
+Route::middleware('auth')->group(function () {
+    Route::get('/reservas/tratamiento/{tratamiento}/profesionales', [ReservaController::class, 'selectProfesional'])
+        ->name('reservas.select-profesional');
+});
 
 require __DIR__ . '/auth.php';
